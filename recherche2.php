1013f9950b759a2f0f4c001ef50d977c69198e02
EmmaDeblois <emma.deblois@etu.univ-lille3.fr>
2017-04-30 11:50:45 +0200

    Ajour page choix
    Ajout de la page choix.html qui sera la première page, celle où l'on
    choisi si on est élève ou enseignant. Il faudra centrer le tableau.
    Création de recherche2.php qui résout le problème de la liste sur le
    choix de l'année, il faut supprimer la ligne vide qui apparait à chaque
    fois.

--- recherche2.php
+++ recherche2.php
@@ -7,20 +7,22 @@
 <h1> Emploi du temps</h1>
 <form method="GET" action="emploi.php">
 <select name="classe">
+<optgroup label="Sélectionner l'année">
 <option value="" selected></option>
 <?php
 $db = new SQLite3('essai.db'); #base de données
 
 $results = $db->query(
-    'SELECT DISTINCT "Annee_detudes", "classe" FROM essai2 ORDER BY "Annee_detudes"' # on met dans la variable results le nom des gares dans l'ordre alphabétique
+    'SELECT DISTINCT "Annee_detudes" FROM essai2 ORDER BY "Annee_detudes"' # on met dans la variable results le nom des gares dans l'ordre alphabétique
 );
 
 while ($row = $results->fetchArray()) { #on met les résultats dans un tableau
     if ($row[0]) {
-        echo "<option value='", $row[1],"'>", $row[0], "</option>\n";
+        echo "<option value='", $row[0],"'>", $row[0], "</option>\n";
     }
 }
 ?>
+</optgroup>
 </select>
 <p>Description d'objet : </p> 
 <input type="text" name="descr"/></br>
