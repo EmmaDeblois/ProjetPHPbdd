573875e486f5ea5a6cba8f921ed22495b24ab862
EmmaDeblois <emma.deblois@etu.univ-lille3.fr>
2017-05-12 14:01:47 +0200

    Commencement du CSS
    CSS sur la page enseignants.php

--- enseignants.php
+++ enseignants.php
@@ -3,32 +3,28 @@
 <head>
    <meta charset="utf-8"/>
      <title>Authentification</title>
+	 <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <BR>
 <center> <h1> Authentification</h1></center>
 <BR>
-<h3>Merci de vous identifier</h3>
+
 <BR>
 <form method="POST" action="authentification.php">
-<table>
 
-<tr>
-<td>
-Votre nom :
-</td>
 
-<td>
-<center>
+<fieldset>
+ <legend>Authentification</legend>
+  <label for="nom">Votre nom :</label>
+		<select name="enseignant">
+			<option value="" selected></option>
+				<?php
+				$db = new SQLite3('enseignantsmdp.db'); #base de données
 
-<select name="enseignant">
-<option value="" selected></option>
-<?php
-$db = new SQLite3('enseignantsmdp.db'); #base de données
-
-$results = $db->query(
-    'SELECT DISTINCT "login" FROM mdp ORDER BY "login"' # on met dans la variable results le nom des gares dans l'ordre alphabétique
-);
+				$results = $db->query(
+					'SELECT DISTINCT "login" FROM mdp ORDER BY "login"' # on met dans la variable results le nom des gares dans l'ordre alphabétique
+				);
 
 while ($row3 = $results->fetchArray()) { #on met les résultats dans un tableau
     if ($row3[0]) {
@@ -36,31 +32,16 @@ while ($row3 = $results->fetchArray()) { #on met les résultats dans un tableau
     }
 }
 ?>
-</select>
+		</select>
 
-</center>
-</td>
-</tr>
+ <p> <label for="mdp">Mot de passe :</label>
+		<input type="password" name="password" /> </p>
+</fieldset>
 
-<tr>
-<td>
-Mot de passe :
-</td>
-
-<td>
-<input type="password" name="password" />
-</td>
-
-</tr>
-
-<tr>
-<td>
+ <p>
 <input type="submit" name="verif" value="Valider"/>
-</td>
+ </p>
 
-</tr>
-
-</table>
 </form>
 </body>
 </html>
